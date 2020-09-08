06feeb147966e919d694a194692a7419ffccfc12
yugo_i <u15jpn@gmail.com>
2020-09-08 17:12:47 +0900

    create counter App

---
+++ src/components/CounterHook.js
@@ -0,0 +1,19 @@
+import React ,{useState}from 'react';
+
+
+function CounterHook() {
+  const [count,setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>カウント+</button>
+    </div>
+  );
+}
+
+export default CounterHook;
