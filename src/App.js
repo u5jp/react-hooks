395bd23bec0e9122443fe74ce92fd2919ba7f372
yugo_i <u15jpn@gmail.com>
2020-09-11 16:09:51 +0900

    useReducer and axios API request

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React, {useState,createContext,useReducer} from 'react';
+import React, {useEffect,useState,createContext,useReducer} from 'react';
 import './App.css';
-// import Counter from './components/Counter'
-// import CounterHook from './components/CounterHook'
-// import FormHook from './components/FomrHook'
-// import ItemHook from './components/ItemHook'
-// import ClassEffect from './components/ClassEffect'
-// import EffectHook from './components/EffectHook'
-// import MouseEventEffect from './components/MouseEventEffect' 
-// import DataFetch from './components/DataFetch'
-// import DataFetchById from './components/DataFetchById'
-// import ComponentC from './components/ComponentC'
-// import CounterReducer from './components/CounterReducer'
-import ComponentA from './components/ComponentA'
-import ComponentB from './components/ComponentB'
-
-// export const UserContext = createContext()
-// export const LanguageContext = createContext()
-
-export const CountContext = createContext()
+import axios from 'axios'
 
 const initialState = {
-  firstCounter:0,
-  secondCounter:10
+  loading:true,
+  error:'',
+  post:{}
 }
+
 const reducer = (state,action)=>{
   switch(action.type){
-    case 'increment1':
-      return { ...state ,firstCounter: state.firstCounter + action.value}
-    case 'decrement1':
-      return { ...state ,firstCounter: state.firstCounter - action.value}
-    case 'reset':
-      return initialState
+    case 'FETCH_SUCCESS':
+      return{
+        loading:false,
+        post:action.payload,
+        error:''
+      }
+    case 'FETCH_ERROR':
+      return{
+        loading:false,
+        post:{},
+        error:'データの取得に失敗しました。'
+      }
     default:
       return state
   }
@@ -38,32 +29,22 @@ const reducer = (state,action)=>{
 
 
 function App() {
-  // const [user, setUser] = useState({name:'yamada',age:'32'})
-  // const [language,setLanguage] = useState('日本語')
-  const [count,dispatch] = useReducer(reducer,initialState)
+  const [state,dispatch] = useReducer(reducer,initialState)
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res => {
+      dispatch({type:'FETCH_SUCCESS',payload:res.data})
+    })
+    .catch(err => {
+      dispatch({type:'FETCH_ERROR'})
+    })
+  })
 
   return (
     <div className="App">
-      <h1>count:{count.firstCounter}</h1>
-      {/* <Counter /> */}
-      {/* <CounterHook /> */}
-      {/* <FormHook />
-      <ItemHook /> */}
-      {/* <ClassEffect/> */}
-      {/* <EffectHook /> */}
-      {/* <MouseEventEffect/> */}
-      {/* <DataFetch /> */}
-      {/* <DataFetchById /> */}
-      {/* <UserContext.Provider value={user}>
-        <LanguageContext.Provider value={language}>
-          <ComponentC />
-        </LanguageContext.Provider>
-      </UserContext.Provider> */}
-      {/* <CounterReducer /> */}
-      <CountContext.Provider value = {{countState:count,countDispatch:dispatch}}>
-        <ComponentA />
-        <ComponentB />
-      </CountContext.Provider>
+      <h1>{state.loading ? 'Loading...':state.post.title}</h1>
+      <h2>{state.error ? state.error:null}</h2>
     </div>
   );
 }
